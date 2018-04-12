68752ccd583f8666afb3e44575b0b302876792d7
Hosana Gomes <hosannagomes@hotmail.com>
2018-04-12 02:12:26 -0300

    Apagando códigos comentados desnecessários

--- main/main.go
+++ main/main.go
@@ -12,11 +12,6 @@ func main() {
 	c := client.ConectarComServidor()
 	menu := Menu{}
 
-	/*var sucesso bool
-
-	sucesso = c.CadastrarNota("2014780267", "AA5879", 8.5)
-	fmt.Println(sucesso)
-*/
 	menu.ExibirMenu(&c)
 	
 }
